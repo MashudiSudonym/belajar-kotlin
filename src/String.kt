6431b004a36e6c73bb1bbd36145d34c7135aa419
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2017-10-17 12:03:14 +0700

    new files

---
+++ src/String.kt
@@ -0,0 +1,13 @@
+fun main (args: Array<String>) {
+    var title = " Selamat datang di unisnu?"
+    println(title)
+    println("title:" + title)
+    println("title: $title")
+    var name = "akhmad" + "khanif"
+    println("name: $name")
+    println("Second char = " + title[1])
+    println(title.toLowerCase())
+    println(title.toUpperCase())
+    println(title.split(" "))
+    println(title.trim())
+}
